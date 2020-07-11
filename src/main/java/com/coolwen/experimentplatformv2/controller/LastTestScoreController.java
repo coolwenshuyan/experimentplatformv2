0559ad69b98e2c9496925109ca2c95102eb0a4b5
CoolWen <122311139@qq.com>
2020-07-11 21:43:38 +0800

    课程整体测试成绩管理，需要测试

--- src/main/java/com/coolwen/experimentplatformv2/controller/LastTestScoreController.java
+++ src/main/java/com/coolwen/experimentplatformv2/controller/LastTestScoreController.java
@@ -2,11 +2,17 @@ package com.coolwen.experimentplatformv2.controller;
 
 import com.coolwen.experimentplatformv2.dao.KaoheModelRepository;
 import com.coolwen.experimentplatformv2.dao.StudentRepository;
+import com.coolwen.experimentplatformv2.model.ArrangeClass;
 import com.coolwen.experimentplatformv2.model.ClassModel;
+import com.coolwen.experimentplatformv2.model.DTO.ArrangeInfoDTO;
 import com.coolwen.experimentplatformv2.model.DTO.StudentLastTestScoreDTO;
+import com.coolwen.experimentplatformv2.model.DTO.StudentReportScoreDTO;
 import com.coolwen.experimentplatformv2.model.Student;
+import com.coolwen.experimentplatformv2.model.User;
+import com.coolwen.experimentplatformv2.service.ArrangeClassService;
 import com.coolwen.experimentplatformv2.service.ClazzService;
 import com.coolwen.experimentplatformv2.service.StudentService;
+import org.apache.shiro.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
@@ -16,10 +22,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.TreeSet;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toCollection;
 
 /**
  * 期末理论测试成绩管理
@@ -43,6 +55,10 @@ public class LastTestScoreController {
     @Autowired
     public ClazzService classService;
 
+
+    @Autowired
+    private ArrangeClassService arrangeClassService;//课程安排表
+
     /**
      * 所有列表
      *
@@ -56,6 +72,8 @@ public class LastTestScoreController {
                                @RequestParam(required = true, defaultValue = "") String select_orderId,
                                @RequestParam(defaultValue = "0", required = true, value = "pageNum") Integer pageNum) {
 
+        User user = (User) SecurityUtils.getSubject().getSession().getAttribute("teacher");
+        logger.debug("登陆用户信息:" + user);
         Page<Student> c = studentService.findStudentPageAndXuehao(pageNum, select_orderId);
 //        //搜索
         logger.debug("c>>>>>>" + c.getSize());
@@ -73,6 +91,10 @@ public class LastTestScoreController {
         logger.debug(">>>>>>>>>>>>>>>>>" + a);
         model.addAttribute("allInfo", a);
 
+        boolean choose = false;
+        model.addAttribute("Choose", choose);
+        List<ArrangeInfoDTO> arrangeInfoDTOs = arrangeClassService.findArrangeInfoDTOByTeacherId(user.getId());
+        model.addAttribute("arrangeInfoDTOs", arrangeInfoDTOs);
         return "kaohe/lastTestScore";
     }
 
@@ -137,5 +159,58 @@ public class LastTestScoreController {
 //        return "kaohe/score_manage";
 //    }
 
+    /**
+     *
+     * @param model
+     * @param arrangeId 安排表id
+     * @param select_orderId 学号
+     * @param pageNum 页码
+     * @return
+     */
+    @GetMapping(value = "/report/{arrangeId}")
+    public String loadOneCourseModel(Model model, @PathVariable int arrangeId, @RequestParam(required = true, defaultValue = "") String select_orderId,
+                                     @RequestParam(defaultValue = "0", required = true, value = "pageNum") Integer pageNum) {
+
+        User user = (User) SecurityUtils.getSubject().getSession().getAttribute("teacher");
+        logger.debug("登陆用户信息:" + user);
+        //所有的下拉列表数据
+        List<ArrangeInfoDTO> arrangeInfoDTOs = arrangeClassService.findArrangeInfoDTOByTeacherId(user.getId());
+        model.addAttribute("arrangeInfoDTOs", arrangeInfoDTOs);
+
+        //当前选择的安排表Id,用于判断按钮跳转连接,以及下拉列表回显
+
+        //判断是否选择了安排
+        boolean choose = true;
+
+        if (arrangeId == -1) {
+            choose = false;
+            model.addAttribute("Choose", choose);
+//            model.addAttribute("selected1", "/report/allModule");
+            return "redirect:/lastTestScoreManage/list";
+        }
+        model.addAttribute("Choose", choose);
+        //本安排的实验模块
+        ArrangeClass arrangeClass = arrangeClassService.findById(arrangeId);
+        logger.debug("安排信息为:" + arrangeClass);
+        int classId = arrangeClass.getClassId();
+
+//        Page<Student> c = studentService.findStudentPageAndXuehao(pageNum, select_orderId);
+        Page<Student> studentPage = studentService.findStudentPageAndXuehaoAndClass(pageNum, classId, select_orderId);
+        logger.debug("当前班级所有学生:" + studentPage.getContent());
+        model.addAttribute("allStu", studentPage);
+        model.addAttribute("selectOrderId", select_orderId);
+        ClassModel classModel = classService.findById(classId);
+        //查询当期班级
+//        List<ClassModel> classList = classService.findCurrentClass();
+        model.addAttribute("classList", classModel);
+
+        //学生成绩DTO列表
+        //todo 是否需要传递其他参数？？？？
+        Page<StudentLastTestScoreDTO> a = studentService.listStudentLastTestAnswerDTO(pageNum);
+        logger.debug("学生成绩信息:" + a);
+        model.addAttribute("allInfo", a);
+
+        return "kaohe/lastTestScore";
+    }
 
 }

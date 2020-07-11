00a504197933a5e72b493839c77f91df4e616651
zhangyunying <1607884857@qq.com>
2020-07-11 22:17:49 +0800

    add

--- src/test/java/com/coolwen/experimentplatformv2/Experimentplatformv2ApplicationTests.java
+++ src/test/java/com/coolwen/experimentplatformv2/Experimentplatformv2ApplicationTests.java
@@ -1,11 +1,15 @@
 package com.coolwen.experimentplatformv2;
 
+import com.coolwen.experimentplatformv2.dao.ExpModelRepository;
 import com.coolwen.experimentplatformv2.dao.StudentRepository;
+import com.coolwen.experimentplatformv2.dao.UserRepository;
 import com.coolwen.experimentplatformv2.service.ArrangeClassService;
+import com.coolwen.experimentplatformv2.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Pageable;
 
 @SpringBootTest
 class Experimentplatformv2ApplicationTests {
@@ -15,6 +19,10 @@ class Experimentplatformv2ApplicationTests {
     ArrangeClassService arrangeClassService;
     @Autowired
     StudentRepository studentRepository;
+    @Autowired
+    ExpModelRepository expModelRepository;
+    @Autowired
+    UserService userRepository;
 
     @Test
     void contextLoads() {
@@ -27,6 +35,8 @@ class Experimentplatformv2ApplicationTests {
 
     @Test
     void test1() {
-        studentRepository.listStudentMTestAnswerDTO();
+//        studentRepository.listStudentMTestAnswerDTO();
+//        System.out.println(expModelRepository.findAllByTeacher(1));
+        System.out.println("————————————————"+userRepository.list());
     }
 }

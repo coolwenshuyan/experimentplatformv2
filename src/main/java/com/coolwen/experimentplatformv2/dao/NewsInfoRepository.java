40d0f87712933298bac8eec57d2e787c23338b2e
zhuzhiwenandzzw <1530448242@qq.com>
2021-01-08 12:30:02 +0800

    v3

--- src/main/java/com/coolwen/experimentplatformv2/dao/NewsInfoRepository.java
+++ src/main/java/com/coolwen/experimentplatformv2/dao/NewsInfoRepository.java
@@ -55,20 +55,32 @@ public interface NewsInfoRepository  extends BaseRepository<NewsInfo, Integer>, 
     //查询学生总成绩排行榜
 //    @Query(value = "select new com.coolwen.experimentplatformv2.model.DTO.OverallScoreDto (s.stuXuehao,avg(avg(tc.totalScore)+avg(tp.totalScore)),l.totalTime) from Student s ,TotalScoreCurrent tc,TotalScorePass tp,LearningTime l where s.id = tc.stuId and tp.stuId = s.id and l.sid = s.id group by s.id")
 //    List<OverallScoreDto> findScoreRanking();
-    @Query(value = "select s.stu_xuehao,ifnull((cc_sumscore+pp_sumscore)/(cc_sumcount+pp_sumcount),0),ifnull(l.total_time,0) \n" +
-            "\n" +
-            "from t_student s LEFT JOIN t_learning_time l on s.id=l.sid \n" +
-            "\n" +
-            "LEFT JOIN ((SELECT stu_id,sum(total_score) AS cc_sumscore,count(stu_id ) as cc_sumcount\n" +
-            "        FROM t_totalscore_current \n" +
-            "        GROUP BY t_totalscore_current.stu_id \n" +
-            ")AS cc_temp) ON s.id=cc_temp.stu_id \n" +
-            "LEFT JOIN ((SELECT stu_id,sum(total_score) AS pp_sumscore,count(stu_id ) as pp_sumcount\n" +
-            "        FROM t_totalscore_pass \n" +
-            "        GROUP BY t_totalscore_pass.stu_id \n" +
-            ")AS pp_temp) on s.id=pp_temp.stu_id \n" +
-            "group by s.id \n" +
-            "order by ifnull((cc_sumscore+pp_sumscore)/(cc_sumcount+pp_sumcount),0) desc,ifnull(l.total_time,0) desc limit 10",nativeQuery=true)
+//    @Query(value = "select s.stu_xuehao,ifnull((cc_sumscore+pp_sumscore)/(cc_sumcount+pp_sumcount),0),ifnull(l.total_time,0) \n" +
+//            "\n" +
+//            "from t_student s LEFT JOIN t_learning_time l on s.id=l.sid \n" +
+//            "\n" +
+//            "LEFT JOIN ((SELECT stu_id,sum(total_score) AS cc_sumscore,count(stu_id ) as cc_sumcount\n" +
+//            "        FROM t_totalscore_current \n" +
+//            "        GROUP BY t_totalscore_current.stu_id \n" +
+//            ")AS cc_temp) ON s.id=cc_temp.stu_id \n" +
+//            "LEFT JOIN ((SELECT stu_id,sum(total_score) AS pp_sumscore,count(stu_id ) as pp_sumcount\n" +
+//            "        FROM t_totalscore_pass \n" +
+//            "        GROUP BY t_totalscore_pass.stu_id \n" +
+//            ")AS pp_temp) on s.id=pp_temp.stu_id \n" +
+//            "group by s.id \n" +
+//            "order by ifnull((cc_sumscore+pp_sumscore)/(cc_sumcount+pp_sumcount),0) desc,ifnull(l.total_time,0) desc limit 10",nativeQuery=true)
+    @Query(value = "select stu_xuehao_a,((cc_sumscore_a+pp_sumscore_a)/(cc_sumcount_a+pp_sumcount_a)),total_time_a from\n" +
+            "(select s.stu_xuehao as stu_xuehao_a,ifnull(cc_sumscore,0) as cc_sumscore_a,ifnull(pp_sumscore,0) as pp_sumscore_a,ifnull(cc_sumcount,0) as cc_sumcount_a,ifnull(pp_sumcount,0) as pp_sumcount_a,ifnull(l.total_time,0) as total_time_a\n" +
+            "from t_student s LEFT JOIN t_learning_time l on s.id=l.sid\n" +
+            "LEFT JOIN ((SELECT stu_id,ifnull(sum(total_score),0) AS cc_sumscore,ifnull(count(stu_id ),0) as cc_sumcount\n" +
+            "        FROM t_totalscore_current\n" +
+            "        GROUP BY t_totalscore_current.stu_id\n" +
+            ")AS cc_temp) ON s.id=cc_temp.stu_id\n" +
+            "LEFT JOIN ((SELECT stu_id,ifnull(sum(total_score),0) AS pp_sumscore,ifnull(count(stu_id),0) as pp_sumcount\n" +
+            "        FROM t_totalscore_pass\n" +
+            "        GROUP BY t_totalscore_pass.stu_id\n" +
+            ")AS pp_temp) on s.id=pp_temp.stu_id) as temp_tb1\n" +
+            "order by ((cc_sumscore_a+pp_sumscore_a)/(cc_sumcount_a+pp_sumcount_a)) desc,total_time_a desc limit 10",nativeQuery=true)
     List findScoreRanking();
 
 //    @Query(value = "select s.stu_xuehao,ifnull(tc.total_score,IFNULL(tp.total_score,0)),ifnull(l.total_time,0) from t_student s LEFT JOIN t_learning_time l on s.id=l.sid LEFT JOIN t_arrange_class ac on s.class_id=ac.class_id and ac.course_id=? LEFT JOIN t_totalscore_current tc ON s.id=tc.stu_id and ac.id=tc.arrange_id LEFT JOIN t_totalscore_pass tp on s.id=tp.stu_id and tp.course_id=ac.course_id ORDER BY ifnull(tc.total_score,IFNULL(tp.total_score,0)) DESC,ifnull(l.total_time,0) DESC limit 14",nativeQuery=true)
